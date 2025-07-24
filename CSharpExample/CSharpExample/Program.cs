4594d1d97dc4a2e4299381e14004fd56ce5296de
김영태 <youngtae.kim@feelaware.com>
2025-07-24 16:31:42 +0900

    [FEATURE] 프로젝트 초기

---
+++ CSharpExample/CSharpExample/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+interface IWeapon
+{
+    string WeaponName { get; set; }
+    int SetDamage { get; set; }
+    int Attack();
+    void Use();
+}
+
+class Sword: IWeapon
+{
+    public string WeaponName { get; set; }
+    public int SetDamage { get; set; }
+    public Sword(int setDamage)
+    {
+        SetDamage = setDamage;
+    }
+
+    public int Attack()
+    {
+        return SetDamage;
+    }
+    
+    public void Use()
+    {
+        Console.WriteLine($"{WeaponName}으로 공격 데미지{Attack}를 입혔습니다.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("hello world");
+        return;
+    }
+}
